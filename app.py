62ebd4cba81a900ad46d46c63798c22191e1b171
fatmatnani <117740815+fatmatnani@users.noreply.github.com>
2024-03-07 21:38:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,63 @@
-# Hacer los importes
-import streamlit as st
+# Importaciones necesarias para la aplicación
+import streamlit as st  # Para animaciones
 from streamlit_lottie import st_lottie
-import pandas as pd
+import pandas as pd  # Para manejar datos
 
-# manajando la base de datos
+# Configuración de la base de datos SQLite
 import sqlite3
-conn = sqlite3.connect('data/world.sqlite')
-c = conn.cursor()
+conn = sqlite3.connect('data/world.sqlite')   # Conexión a la base de datos
+c = conn.cursor()   # Crear un cursor para ejecutar consultas
 
-#fxn make execution
+# Función para ejecutar código SQL raw y devolver los resultados
 def sql_executor(raw_code):
-    c.execute(raw_code)
-    data = c.fetchall()
-    return data
+    c.execute(raw_code)  # Ejecuta el código SQL
+    data = c.fetchall()  # Recoge los resultados
+    return data   # Devuelve los resultados
 
+# Listas que contienen los campos de las tablas disponibles para consultas
 city = ['ID,', 'Name,', 'CountryCode,', 'District,', 'Population']
 country = ['Code,', 'Name,', 'Continent,', 'Region,', 'SurfaceArea,', 'IndepYear,', 'Population,', 'LifeExpectancy,', 'GNP,', 'GNPOld,', 'LocalName,', 'GovernmentForm,', 'HeadOfState,', 'Capital,', 'Code2']
 countrylanguage = ['CountryCode,', 'Language,', 'IsOfficial,', 'Percentage']
 
 
 def main():
-    st.title("SQL Playground")
-    st_lottie('https://lottie.host/6f4ee854-3625-4849-8985-e423a0752949/hZWZsrb0zk.json', key="user")
-    menu = ["Home", "About"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    st.title("SQL Playground")  # Título de la aplicación
+    st_lottie('https://lottie.host/6f4ee854-3625-4849-8985-e423a0752949/hZWZsrb0zk.json', key="user")   # Carga una animación de Lottie
+    menu = ["Home", "About"]  # Menú lateral para navegar
+    choice = st.sidebar.selectbox("Menu", menu)   # Selector del menú
 
-    if choice == "Home":
+    if choice == "Home":   # Si se elige Home
         st.subheader("Homepage")
         # Columnas de la pagina
-        col1,col2 = st.columns(2)
-        with col1:
-            with st.form(key='query form'):
-                raw_code = st.text_area("SQL CODE Here")
-                submit_code = st.form_submit_button("Execute")
-                # Tabla de info
+        col1,col2 = st.columns(2)  # Divide la pantalla en dos columnas
+        with col1:  # Primera columna
+            with st.form(key='query form'):   # Formulario para la entrada de la consulta SQL
+                raw_code = st.text_area("SQL CODE Here")  # Área de texto para ingresar el código SQL
+                submit_code = st.form_submit_button("Execute")  # Botón para ejecutar la consulta
+                # # Expansor para mostrar información de las tablas
                 with st.expander("Table Info"):
                     table_info = {'city':city,'country':country,'countrylanguage':countrylanguage}
-                    st.json(table_info)
+                    st.json(table_info) # Muestra la información de las tablas en formato JSON
 				    
                     
-                # resulta
-                with col2:
-                    if submit_code:
+                # # Segunda columna
+                with col2: 
+                    if submit_code:  # Si se ha enviado el código
                         st.info("Query Submitted")
-                        st.code(raw_code)
+                        st.code(raw_code)   # Muestra el código SQL enviado
 
                         # results
-                        query_results = sql_executor(raw_code)
-                        with st.expander("Results)"):
+                        query_results = sql_executor(raw_code)  # Ejecuta la consulta y obtiene los resultados
+                        with st.expander("Results)"):   # Expansor para mostrar los resultados crudos
                             st.write(query_results)
 
-                        with st.expander("Pretty Table"):
-                            query_df = pd.DataFrame(query_results)
-                            st.dataframe(query_df)    
+                        with st.expander("Pretty Table"): # Expansor para mostrar los resultados en una tabla bonita
+                            query_df = pd.DataFrame(query_results)  # Convierte los resultados en un DataFrame de Pandas
+                            st.dataframe(query_df)  # Muestra el DataFrame  
 
-    else:
-        st.subheader("Speak with your DB")
+    else:  # Si se elige About
+        st.subheader("Speak with your DB")  # Subtítulo de la sección About
 
 
 if __name__== '__main__':
-    main()            
+    main()            # Ejecuta la función principal
